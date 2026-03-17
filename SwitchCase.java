2a1c4238db14ebdd347bad7c03fa5cbaa8ca2083
matheussantos4 <matheus.santos88y@gmail.com>
2026-03-17 18:46:21 -0300

    SwitchCase

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,7 +1,49 @@
+
+import java.util.Scanner;
+
 public class SwitchCase {
 
     public static void main(String[] args) {
 
-    }
+        // Toda estrutura poderia ser realizada com if-else, mas essa é uma das
+        // alternativas possíveis.
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Número correspondente ao dia da semana: ");
+
+        int x = sc.nextInt();
+
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terça";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+
+            default:
+                dia = "valor invalido";
+        }
+
+        System.out.println("Dia da semana " + dia);
+        sc.close();
+    }
 }
