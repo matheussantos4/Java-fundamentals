da322b49aa3daed7351539ad9a2a8f0d2bad0a53
matheussantos4 <matheus.santos88y@gmail.com>
2026-04-02 20:02:36 -0300

    init conversor temperatura

---
+++ exerciciosDoWhile/farenheit.java
@@ -0,0 +1,45 @@
+package exerciciosDoWhile;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class farenheit {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.of("pt", "BR"));
+
+        int op = 0;
+        double fh = 0;
+        double C = 0;
+
+        do {
+            System.out.println("Selecione a conversão: Farenheit para Celcius(.1)");
+            System.out.println("Celcius para Farenheit(.2)");
+            System.out.println("Sair(3)");
+            op = sc.nextInt();
+
+            if (op == 1) {
+
+                System.out.println("Digite a temperatura em Farenheit: ");
+                fh = sc.nextInt();
+                C = (fh - 32) / 1.8;
+
+                System.out.println("Temperatura em Celcius: %.2f%n" + C);
+
+            } else if (op == 2) {
+                System.out.println("Digite a temperatura em Celcius:");
+                C = sc.nextDouble();
+                fh = (C * 1.8) + 32;
+
+                System.out.println("Temperatura em Ferenheit: %.2f%n" + fh);
+
+            } else
+                System.out.println("Opção inválida.");
+
+        } while (op != 3);
+
+        System.out.println("Muito obrigado !");
+        sc.close();
+    }
+}
